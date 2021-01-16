1987873a46a8bccdc19998d559ab14653d108f52
Amatullah Brown <amatullahbrown@Amatullahs-MBP.fios-router.home>
2021-01-16 12:09:44 -0500

    updated to show F, C, K, and R values

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
 const button = document.getElementById('buttonId');
-const finalResult = document.getElementById('result');
+const fahrenheitResult = document.getElementById('fahrenheitResult');
+const celsiusResult = document.getElementById('celsiusResult');
+const kelvinResult = document.getElementById('kelvinResult');
+const rankineResult = document.getElementById('rankineResult');
 
+
+function calculate() {
+    const value = document.getElementById('value').value;
+    const scale = document.getElementById('scale').value;
+    if(scale == "fahrenheit") {
+        fahrenheitResult.innerHTML = value + " F";
+        let celsius = (value - 32) * 5.0 / 9.0;
+        celsiusResult.innerHTML = celsius + " C";
+        kelvinResult.innerHTML = (celsius + 273.15).toFixed(2) + " K";
+        rankineResult.innerHTML = (parseFloat(value) + 459.67).toFixed(2);
+    } else if (scale == "celsius") {
+        let fahrenheit = value * 9.0 / 5.0 + 32;
+        fahrenheitResult.innerHTML = fahrenheit + " F";
+        celsiusResult.innerHTML = value + " C";
+        kelvinResult.innerHTML = (parseFloat(value) + 273.15).toFixed(2) + " K";
+        rankineResult.innerHTML = (fahrenheit + 459.67).toFixed(2) + " R";
+    } else if (scale == "kelvin") {
+        let fahrenheit = ((parseFloat(value) - 273.15) * 9.0 / 5.0 + 32);
+        fahrenheitResult.innerHTML = fahrenheit.toFixed(2)  + " F";
+        celsiusResult.innerHTML = value - 273.15 + " C";
+        kelvinResult.innerHTML = value + " K";
+        rankineResult.innerHTML = (fahrenheit + 459.67).toFixed(2) + " R";
+    } else if (scale == "rankine") {
+        fahrenheitResult.innerHTML = (value - 459.67).toFixed(2) + " F";
+        let celsius = (value - 491.67) + 5.0 / 9.0;
+        celsiusResult.innerHTML = celsius.toFixed(2) + " C";
+        kelvinResult.innerHTML = (celsius + 273.15).toFixed(2) + " K";
+        rankineResult.innerHTML =  value + " R";
+    }
+}
 
 button.addEventListener('click', () => {
-    const value = document.getElementById('value').value;
-    let result = (value - 32) * 5.0 / 9.0;
-    finalResult.innerHTML = result;
+    fahrenheitResult.innerHTML = "";
+    celsiusResult.innerHTML = "";
+    kelvinResult.innerHTML = "";
+    rankineResult.innerHTML = "";
+    calculate();
+});
+
+document.getElementById('value').addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        fahrenheitResult.innerHTML = "";
+        celsiusResult.innerHTML = "";
+        kelvinResult.innerHTML = "";
+        rankineResult.innerHTML = "";
+        calculate();
+    }
 });
